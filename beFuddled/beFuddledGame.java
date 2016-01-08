8c69aafdd582c4287f24f8fee4c23f5bfb2b18e9
Yang Pan <ypan01@hotmail.com>
2016-01-07 22:34:33 -0800

    Most features of BeFuddled implemented
    BeFuddledGen now generates moves for each game log, can create new game
    logs and creates as many game logs as specified from the command line
    
    Still need to add the type of special move names with an algorithm
    guaranteeing that each special move type is between 15% to 33% usage

--- beFuddled/beFuddledGame.java
+++ beFuddled/beFuddledGame.java
@@ -1,9 +1,14 @@
+import java.util.Random;
+import java.util.ArrayList;
 
 class beFuddledGame {
 	private String userId;
 	private int gameId;
 	private int points;
 	private int actionNum;
+	private int totalMoves;
+	private int totalSpecialMoves;
+	private ArrayList<Integer> specialMoveIndices;
 
 	public beFuddledGame() {
 		userId = "";
@@ -17,6 +22,14 @@ class beFuddledGame {
 		gameId = game;
 		points = 0;
 		actionNum = 1;
+	}
+
+	public int getTotalSpecialMoves() {
+		return totalSpecialMoves;
+	}
+
+	public int getTotalMoves() {
+		return totalMoves;
 	}
 
 	public String getUserId() {
@@ -49,5 +62,35 @@ class beFuddledGame {
 
 	public void updateActionNum() {
 		++actionNum;
+	}
+
+	public void setTotalMoves(int moves) {
+		totalMoves = moves;
+	}
+
+	public void setTotalSpecialMoves(int num) {
+		totalSpecialMoves = num;
+	}
+
+	public void createSpecialMoveIndices() {
+		Random rand = new Random();
+
+		specialMoveIndices = new ArrayList<Integer>();
+		Integer index;
+		for (int i = 0; i < totalSpecialMoves; i++) {
+			index = new Integer(rand.nextInt(totalMoves - 1 + 1) + 1);
+			while (specialMoveIndices.contains(index)) {
+				index = new Integer(rand.nextInt(totalMoves - 1 + 1) + 1);
+			}
+			specialMoveIndices.add(index);
+		}
+		
+	}
+
+	public boolean isSpecialMove() {
+		if (specialMoveIndices.contains(new Integer(actionNum))) {
+			return true;
+		}
+		return false;
 	}
 }
--- beFuddled/beFuddledGen.java
+++ beFuddled/beFuddledGen.java
@@ -4,9 +4,13 @@ import java.util.*;
 
 class beFuddledGen {
 
-    private static HashMap<String, beFuddledGame> currentGames = new HashMap<String, beFuddledGame>();
+    private static HashMap<Integer, beFuddledGame> currentGames = new HashMap<Integer, beFuddledGame>();
     private static HashMap<String, String> currentUsers = new HashMap<String, String>();
     private static int gameCount = 1;
+    private static int gameCap;
+    private static int currentLogEvents;
+    private static int maxGameId;
+    private static int minGameId;
 
     /* Returns a random number in the range specified by min and max
         with min and max inclusive */
@@ -33,19 +37,7 @@ class beFuddledGen {
         return tempUserId;
     }
 
-    /*public static JSONObject createActionObject() {
-        int moveType = randomNumber(1, 100);
-
-        if (moveType <= 90) {
-            //regular move
-
-        }
-        else {
-            //special move
-        }
-    }*/
-
-    public static JSONObject createGameStart() {
+    public static JSONObject createGameStartAction() {
         JSONObject gameStart = new JSONObject();
         try {
             gameStart.put("actionNumber", 1);
@@ -63,7 +55,7 @@ class beFuddledGen {
         try {
             startRecord.put("user", user);
             startRecord.put("game", game);
-            JSONObject startAction = createGameStart();
+            JSONObject startAction = createGameStartAction();
             startRecord.put("action", startAction);
         }
         catch (Exception e) {
@@ -73,16 +65,211 @@ class beFuddledGen {
         return startRecord;
     }
 
-    public static JSONObject createMoveLogRecord() {
-        JSONObject logRecord = new JSONObject();
+    /* Finds the total number of moves a game will have, based on the probabilities
+        specified in the README */
+    public static int totalGameMoves() {
+        int movePercentage = randomNumber(1, 100);
+
+        if (movePercentage <= 10) {
+            return randomNumber(9, 20);
+        }
+        else if (movePercentage > 10 && movePercentage <= 30) {
+            return randomNumber(21, 30);
+        }
+        else if (movePercentage > 30 && movePercentage <= 80) {
+            return randomNumber(31, 50);
+        }
+        else if (movePercentage > 80 && movePercentage <= 95) {
+            return randomNumber(51, 70);
+        }
+        else {
+            return randomNumber(71, 100);
+        }
+    }
+
+    /* Finds the total number of special moves a game will have based on the total
+        moves that game has as specified in the README */
+    public static int totalSpecialMoves(int totalMoves) {
+        if (totalMoves >= 9 && totalMoves <= 30) {
+            return 1;
+        }
+        else if (totalMoves >= 31 &&  totalMoves <= 50) {
+            return 2;
+        }
+        else {
+            return 3;
+        }
+    }
+
+    /* Checks if the next action should be starting a new game. 
+        Only start if a new game if the currentGames is less than the gameCap
+        AND the random number generator is between 71-100 */
+    public static boolean shouldStartNewGame() {
+        if (currentGames.size() == gameCap) {
+            return false;
+        }
+        else {
+            int startPercentage = randomNumber(1, 100);
+            if (startPercentage <= 70) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+
+    public static JSONObject initializeNewGameStart() {
+        String user = getUser();
+        int gameNum = gameCount++;
+
+        JSONObject startLog = createStartLogRecord(user, gameNum);
+        beFuddledGame gameObj = new beFuddledGame(user, gameNum);
+        gameObj.updateActionNum();
+
+        int totalMoves = totalGameMoves();
+        int totalSpecials = totalSpecialMoves(totalMoves);
+        gameObj.setTotalMoves(totalMoves);
+        gameObj.setTotalSpecialMoves(totalSpecials);
+        gameObj.createSpecialMoveIndices();
+
+        currentGames.put(new Integer(gameNum), gameObj);
+
+        return startLog;
+    }
+
+    /* Finds a random game id that is currently ongoing for the next move */
+    public static int getGameIdToPlay() {
+        int tempGameId = randomNumber(minGameId, maxGameId);
+
+        while (!currentGames.containsKey(new Integer(tempGameId))) {
+            tempGameId = randomNumber(minGameId, maxGameId);
+        }
+
+        return tempGameId;
+    }
+
+    public static JSONObject createGameEndAction(int actionNum, int points) {
+        JSONObject endAction = new JSONObject();
         try {
-            
+            endAction.put("actionNumber", actionNum);
+            endAction.put("actionType", "GameEnd");
+            endAction.put("points", points);
+            if (randomNumber(0, 1) == 1) {
+                endAction.put("gameStatus", "Win");
+            }
+            else {
+                endAction.put("gameStatus", "Loss");
+            }
         }
         catch (Exception e) {
 
         }
 
-        return logRecord;
+        return endAction;
+    }
+
+    public static JSONObject createMoveLocation() {
+        JSONObject location = new JSONObject();
+        try {
+            int dice = randomNumber(1, 100);
+            if (dice <= 3) {
+                location.put("x", randomNumber(1, 2));
+            }
+            else if (dice > 3 && dice <= 15) {
+                location.put("x", randomNumber(3, 6));
+            }
+            else if (dice > 15 && dice <= 85) {
+                location.put("x", randomNumber(7, 13));
+            }
+            else if (dice > 85 && dice <= 97) {
+                location.put("x", randomNumber(14, 17));
+            }
+            else {
+                location.put("x", randomNumber(18, 20));
+            }
+
+            int dice2 = randomNumber(1, 100);
+            if (dice2 <= 3) {
+                location.put("y", randomNumber(1, 2));
+            }
+            else if (dice2 > 3 && dice2 <= 15) {
+                location.put("y", randomNumber(3, 6));
+            }
+            else if (dice2 > 15 && dice2 <= 85) {
+                location.put("y", randomNumber(7, 13));
+            }
+            else if (dice2 > 85 && dice2 <= 97) {
+                location.put("y", randomNumber(14, 17));
+            }
+            else {
+                location.put("y", randomNumber(18, 20));
+            }
+        }
+        catch (Exception e) {
+
+        }
+
+        return location;
+    }
+
+    public static JSONObject createRegMoveLogRecord(beFuddledGame gameObj) {
+        JSONObject moveRecord = new JSONObject();
+        try {
+            String userId = gameObj.getUserId();
+            int gameId = gameObj.getGameId();
+            moveRecord.put("user", userId);
+            moveRecord.put("game", gameId);
+
+            JSONObject regMoveAction = new JSONObject();
+            int actionNum = gameObj.getActionNum();
+            int oldPoints = gameObj.getPoints();
+            regMoveAction.put("actionNumber", actionNum);
+            regMoveAction.put("actionType", "Move");
+            int pointsAdded = randomNumber(-20, 20);
+            regMoveAction.put("pointsAdded", pointsAdded);
+            regMoveAction.put("points", oldPoints + pointsAdded);
+            regMoveAction.put("location", createMoveLocation());
+
+            gameObj.updateActionNum();
+            gameObj.updatePoints(pointsAdded);
+
+
+            moveRecord.put("action", regMoveAction);
+        }
+        catch (Exception e) {
+
+        }
+
+        return moveRecord;
+    }
+
+    public static JSONObject createSpecialMoveLogRecord(beFuddledGame gameObj) {
+        JSONObject moveRecord = new JSONObject();
+        try {
+            moveRecord.put("user", gameObj.getUserId());
+            moveRecord.put("game", gameObj.getGameId());
+
+            JSONObject specialMoveAction = new JSONObject();
+            specialMoveAction.put("actionNumber", gameObj.getActionNum());
+            specialMoveAction.put("actionType", "SpecialMove");
+            int pointsAdded = randomNumber(-20, 20);
+            specialMoveAction.put("pointsAdded", pointsAdded);
+            specialMoveAction.put("points", gameObj.getPoints() + pointsAdded);
+
+            //TODO: implement the probability for what type of special move it is
+            //specialMoveAction.put("move", ...);
+
+            gameObj.updateActionNum();
+            gameObj.updatePoints(pointsAdded);
+
+            moveRecord.put("action", specialMoveAction);
+        }
+        catch (Exception e) {
+
+        }
+
+        return moveRecord;
     }
 
 
@@ -101,16 +288,48 @@ class beFuddledGen {
                 JSONArray outputLog = new JSONArray();
 
                 int startGames = initialOngoingGames(logTotal);
+                gameCap = startGames;
+
+                //Create the initial Game Start logs and construct their associated
+                //beFuddledGame objects
                 for (int i = 1; i <= startGames; i++) {
-                    String user = getUser();
-                    int gameNum = gameCount++;
-                    JSONObject startLog = createStartLogRecord(user, gameNum);
-                    beFuddledGame gameObj = new beFuddledGame(user, gameNum);
-                    gameObj.updateActionNum();
-
-                    currentGames.put(Integer.toString(i), gameObj);
-
+                    JSONObject startLog = initializeNewGameStart();
                     outputLog.put(startLog);
+                }
+
+                minGameId = 1;
+                maxGameId = gameCap;
+
+                //update the number of log events that have occurred
+                currentLogEvents = startGames;
+
+                //Create the rest of the log events as either existing game moves,
+                //ending a game, or starting a new game
+                while (currentLogEvents < logTotal) {
+                    if (shouldStartNewGame()) {
+                        JSONObject newStartLog = initializeNewGameStart();
+                        outputLog.put(newStartLog);
+
+                        //gameCount was incremented in initializeNewGameStart. 
+                        //decrement it by one to get the current max game id that 
+                        //was just created.
+                        maxGameId = gameCount - 1;
+                    }
+                    else {
+                        int gameIdToPlay = getGameIdToPlay();
+                        beFuddledGame gameObjToPlay = currentGames.get(gameIdToPlay);
+
+                        if (gameObjToPlay.isSpecialMove()) {
+                            JSONObject specialMoveLog = createSpecialMoveLogRecord(gameObjToPlay);
+                            outputLog.put(specialMoveLog);
+                        }
+                        else {
+                            JSONObject regMoveLog = createRegMoveLogRecord(gameObjToPlay);
+                            outputLog.put(regMoveLog);
+                        }
+                    }
+
+                    ++currentLogEvents;
                 }
 
                 File outputFile = new File(fileName);
